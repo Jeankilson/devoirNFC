4c75a183b824703f35771fc7911526494d240b01
jeankilson <cinotech19@gmail.com>
2018-04-16 20:04:06 -0700

    envoie du devoir NFenvoie du devoir NFenvoie du devoir NFCCC

---
+++ app/src/main/java/com/brett/beam/MessageNFC.java
@@ -0,0 +1,43 @@
+package com.brett.beam;
+
+/**
+ * Created by Joel Felix St Joy
+ * on 12/04/218.
+ */
+
+
+public class MessageNFC {
+    private String from;
+    private String message;
+    private String mDate;
+
+    public MessageNFC(String from, String message, String mDate) {
+        this.from = from;
+        this.message = message;
+        this.mDate = mDate;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+}
